245b60864230565b81a92535121b3c81d4032c05
Naghmeh <naghmeh.majidpour@gmail.com>
2020-04-18 18:16:57 -0400

    restructure navigation

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,34 @@
 import React from "react";
-import "./App.css";
-import Navigation from './components/navigation';
+import { BrowserRouter as Router, NavLink, Route } from "react-router-dom";
+
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+import Counter from "./components/Counter";
+import MyList from "./components/MyList";
+import './App.css';
 
 function App() {
   return ( 
-    <Navigation/>
+    <Router>
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand as={NavLink} to="/">
+          My React App
+        </Navbar.Brand>
+        <Nav className="ml-auto">
+          <Nav.Link as={NavLink} to="/mylist">
+            Lists
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/cart">
+            Cart
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+      <Route path="/mylist" exact component={MyList} />
+      <Route path="/cart" exact component={Counter} />
+    </div>
+  </Router>
   );
 }
 
---
+++ my-react-app/src/components/MyList.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class MyList extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="container">
+        <h6>My Favourites!</h6>
+      </div>
+    );
+  }
+}
+
+export default MyList;
