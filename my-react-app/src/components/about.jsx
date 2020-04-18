c5ef9d82dd854071301fe1d6013b0d29e560353a
Naghmeh <naghmeh.majidpour@gmail.com>
2020-04-18 13:21:41 -0400

    fixed navigation issue

--- my-react-app/src/components/about.jsx
+++ my-react-app/src/components/about.jsx
@@ -4,7 +4,10 @@ class About  extends Component {
     state = {  }
     render() { 
         return ( 
+            <div className="container">
             <h6>Welcome to my react app!</h6>
+            <p>This is my practicing react application.</p>
+            </div>
          );
     }
 }
--- my-react-app/src/components/navigation.jsx
+++ my-react-app/src/components/navigation.jsx
@@ -1,36 +1,33 @@
 import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import Form from "react-bootstrap/Form";
-import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, NavLink, Route } from "react-router-dom";
 import Counter from "./counter";
 import About from "./about";
-import App from "../App";
 
 class Navigation extends Component {
   state = {};
   render() {
     return (
-      <Navbar bg="dark" variant="dark">
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
-        <Nav className="ml-auto">
-          <Router>
-            <Nav.Link><Link to="counter">Cart</Link></Nav.Link> 
-            <Nav.Link><Link to="about">About</Link></Nav.Link>
-            <Switch>
-              <Route path="{/}" component={App}>
-                <Route path="/counter" component={Counter}></Route>
-                <Route path="/about" component={About}></Route>
-              </Route>
-            </Switch>
-          </Router>
-        </Nav>
-      </Navbar>
+      <Router>
+        <div className="App">
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand as={NavLink} to="/">
+              My React App
+            </Navbar.Brand>
+            <Nav className="ml-auto">
+              <Nav.Link as={NavLink} to="/about">
+                About
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/cart">
+                Cart
+              </Nav.Link>
+            </Nav>
+          </Navbar>
+          <Route path="/about" exact component={About} />
+          <Route path="/cart" exact component={Counter} />
+        </div>
+      </Router>
     );
   }
 }
