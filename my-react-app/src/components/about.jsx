2cdfe939caa49c3328d89addcad3bf7e78719f53
Naghmeh <naghmeh.majidpour@gmail.com>
2020-04-18 11:37:29 -0400

    added routing

---
+++ my-react-app/src/components/about.jsx
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+
+class About  extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <h6>Welcome to my react app!</h6>
+         );
+    }
+}
+ 
+export default About;
--- my-react-app/src/components/navigation.jsx
+++ my-react-app/src/components/navigation.jsx
@@ -1,9 +1,13 @@
-import React , { Component } from "react";
+import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Counter from "./counter";
+import About from "./about";
+import App from "../App";
 
 class Navigation extends Component {
   state = {};
@@ -15,8 +19,16 @@ class Navigation extends Component {
           <Button variant="outline-info">Search</Button>
         </Form>
         <Nav className="ml-auto">
-          <Nav.Link>My List</Nav.Link>
-          <Nav.Link>Cart</Nav.Link>
+          <Router>
+            <Nav.Link><Link to="counter">Cart</Link></Nav.Link> 
+            <Nav.Link><Link to="about">About</Link></Nav.Link>
+            <Switch>
+              <Route path="{/}" component={App}>
+                <Route path="/counter" component={Counter}></Route>
+                <Route path="/about" component={About}></Route>
+              </Route>
+            </Switch>
+          </Router>
         </Nav>
       </Navbar>
     );
